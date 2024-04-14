1d182fd2c6c9ec8d78eeb227526effef91df2cc2
tanoj sai praveen <tanojsaipraveench@outlook.com>
2024-04-14 08:57:51 +0530

    init

---
+++ src/routes/albums.route.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express'
+import { AlbumsController } from '../controllers/albums.controller'
+import { albumsSchema } from '../helpers/validation.helper'
+import type { Route } from '../interfaces/route.interface'
+
+export class AlbumsRoute implements Route {
+  public path = '/albums'
+  public router = Router()
+  public albumsController = new AlbumsController()
+
+  constructor() {
+    this.initializeRoutes()
+  }
+
+  private initializeRoutes() {
+    console.log(this.path);
+    this.router.get(`${this.path}`, albumsSchema, this.albumsController.albumDetails)
+  }
+}
